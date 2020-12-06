fc9524ec8ec26127ab5da97f56c8a238bbd3f976
Hartz-I <ahnafsadikinan@gmail.com>
2020-12-06 22:40:03 +0600

    building search component design

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import logo from './logo-borgomul-triangle.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo-borgomul-triangle.svg";
+import "./App.css";
+import SearchBar from "./components/SearchBar/SearchBar.component";
 
 function App() {
   return (
     <div className="App">
+      <SearchBar
+        placeholder="Search for content..."
+        color="#407B58"
+        changed={null}
+      />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          বর্গমূল
-        </div>
+        <div>বর্গমূল</div>
       </header>
     </div>
   );
